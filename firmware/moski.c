d3e021cd8005f6f63b095000374b77058ee90dea
Edgar Simo <bobbens@gmail.com>
2009-04-21 19:22:31 +0200

    * Simple program to test communication.

--- firmware/moski.c
+++ firmware/moski.c
@@ -1,20 +1,53 @@
 
-
-/* Set clock speed. */
-#define F_CPU 1000000UL
-
+#include "moski.h"
 
 #include <avr/io.h>
 #include <util/delay.h>
 
+#include <stdint.h>
 
-static void delay (void)
+#include "i2cs.h"
+
+
+static uint8_t moski_mode = 0x00; /**< Current operating mode. */
+
+
+/*
+ * Prototypes.
+ */
+static uint8_t moski_read( uint8_t pos, uint8_t value );
+static uint8_t moski_write( uint8_t buf_len, uint8_t *buffer );
+
+
+/**
+ * @brief Master read function for the Moski.
+ *
+ *    @param pos Position of byte read.
+ *    @param value Value of byte read.
+ *    @return 0 to send ACK, 1 to send NACK.
+ */
+static uint8_t moski_read( uint8_t pos, uint8_t value )
 {
-   _delay_ms( 3000. );
-   _delay_ms( 3000. );
-   _delay_ms( 3000. );
-   _delay_ms( 3000. );
-   _delay_ms( 3000. );
+   (void) pos;
+
+   moski_mode = value;
+   return 0;
+}
+
+
+/**
+ * @brief Master write function for the Moski.
+ *
+ *    @param buf_len Maximum buffer length.
+ *    @param buffer Buffer to write.
+ *    @return Number of bytes to write.
+ */
+static uint8_t moski_write( uint8_t buf_len, uint8_t *buffer )
+{
+   (void) buf_len;
+
+   buffer[0] = moski_mode;
+   return 1;
 }
 
 
@@ -23,31 +56,12 @@ static void delay (void)
  */
 int main (void)
 {
-   /* Output for PWM. */
-   DDRB  = _BV(DDB2);
-   /* Output for direction. */
-   DDRA  = _BV(DDA3);
+   /* Set up communication. */
+   i2cs_setAddress( 0x09 );
+   i2cs_setReadCallback( moski_read );
+   i2cs_setWriteCallback( moski_write );
+   i2cs_init();
 
-   while (1) {
-      /* Motors off. */
-      PORTB = 0;
-      PORTA = 0;
-      delay();
-
-      /* Motors forward. */
-      PORTB = _BV(PB2);
-      PORTA = 0;
-      delay();
-
-      /* Motors off. */
-      PORTB = 0;
-      PORTA = 0;
-      delay();
-
-      /* Motors forward. */
-      PORTB = 0;
-      PORTA = _BV(PA3);
-      delay();
-   }
+   while (1) {}
 }
 
