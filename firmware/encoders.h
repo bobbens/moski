a33951519bee0c9c1cba2065f09a5062be5d3c52
Edgar Simo <bobbens@gmail.com>
2009-05-19 20:50:55 +0200

    * Changed encoder pins.

--- firmware/encoders.h
+++ firmware/encoders.h
@@ -13,10 +13,10 @@
 #define ENCODER_INT_B      PCINT1   /**< Interrupt pin encoder B is on. */
 #define ENCODER_SIG        SIG_PIN_CHANGE0 /**< Interrupt vector for encoders. */
 
-#define ENCODER_DDR        DDRB     /**< DDR register for encoders. */
-#define ENCODER_PIN        PINB     /**< Pin encoders are on. */
-#define ENCODER_PORT_A     PB0      /**< Encoder A port. */
-#define ENCODER_PORT_B     PB1      /**< Encoder B port. */
+#define ENCODER_DDR        DDRA     /**< DDR register for encoders. */
+#define ENCODER_PIN        PINA     /**< Pin encoders are on. */
+#define ENCODER_PORT_A     PA0      /**< Encoder A port. */
+#define ENCODER_PORT_B     PA1      /**< Encoder B port. */
 #define ENCODER_PIN_A      ENCODER_PORT_A /**< Encoder A pin. */
 #define ENCODER_PIN_B      ENCODER_PORT_B /**< Encoder B pin. */
 
